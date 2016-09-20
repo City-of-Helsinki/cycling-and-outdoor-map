4f4d3b7413336012ed34abd65597322848ceb1ea
Jonne <jonne.tanninen@gmail.com>
2016-09-20 13:00:23 +0300

    Route visible after finishing measuring

--- src/components/measureTool.js
+++ src/components/measureTool.js
@@ -41,6 +41,27 @@ var continueLineMsg = 'Anna seuraava mittauspiste, tuplaklikki lopettaa mittauks
 
 var source = new ol.source.Vector();
 
+var vector = new ol.layer.Vector({
+  source: source,
+  style: new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: 'rgba(255, 255, 255, 0.2)'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#ffcc33',
+      width: 4
+    }),
+    image: new ol.style.Circle({
+      radius: 7,
+      fill: new ol.style.Fill({
+        color: '#ffcc33'
+      })
+    })
+  })
+});
+
+Map.addLayer(vector);
+
 var pointerMoveHandler = function(evt) {
 
   if (!measuringEnabled) {
