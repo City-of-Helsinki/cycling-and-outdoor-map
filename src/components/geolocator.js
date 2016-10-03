4c50f53718144a445dfccddb86d8b0f341288ef2
Jonne <jonne.tanninen@gmail.com>
2016-10-03 14:17:37 +0300

    Added icon for geolocation

--- src/components/geolocator.js
+++ src/components/geolocator.js
@@ -1,14 +1,16 @@
 import ol from 'openlayers';
-//import walkImg from 'assets/img/walk32.png';
-import projection from './projection';
+import walkImg from 'assets/img/walk32.png';
+//import projection from './projection';
 import { Map } from './map';
+
+let lastIcon;
 
 export const setGeoLocation = () => {
   const geolocation = new ol.Geolocation({
     tracking: true,
     projection
   });
-  /*const iconStyle = new ol.style.Style({
+  const iconStyle = new ol.style.Style({
     image: new ol.style.Icon(({
       anchor: [0.5, 15],
       anchorXUnits: 'fraction',
@@ -21,18 +23,25 @@ export const setGeoLocation = () => {
   const accuracyFeature = new ol.Feature();
   const positionFeature = new ol.Feature();
 
-  positionFeature.setStyle(iconStyle);*/
+  positionFeature.setStyle(iconStyle);
 
   let locationCoordinates = geolocation.getPosition();
   Map.getView().setCenter(locationCoordinates);
   Map.getView().setZoom(5);
-  //positionFeature.setGeometry(locationCoordinates ? new ol.geom.Point(locationCoordinates) : null);
-  /*new ol.layer.Vector({
+  positionFeature.setGeometry(locationCoordinates ? new ol.geom.Point(locationCoordinates) : null);
+  removeLastIcon();
+  var vectorLayer = new ol.layer.Vector({
     map: Map,
     source: new ol.source.Vector({
       features: [accuracyFeature, positionFeature]
     })
-  });*/
+  });
+  lastIcon = vectorLayer;
+  Map.addLayer(vectorLayer);
 
   setTimeout(function(){ Map.updateSize(); }, 100);
+};
+
+const removeLastIcon = () => {
+  Map.removeLayer(lastIcon);
 };
