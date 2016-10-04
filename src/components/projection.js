f4a72a6f2097bc1668e5d48ed6eb48782e2ba57b
Jonne <jonne.tanninen@gmail.com>
2016-10-04 13:01:53 +0300

    Fixed geolocation

--- src/components/projection.js
+++ src/components/projection.js
@@ -1,6 +1,7 @@
 import ol from 'openlayers';
 import proj4 from 'proj4';
 
+ol.proj.setProj4(proj4);
 proj4.defs('EPSG:3879', '+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs');
 
 const projection = new ol.proj.Projection({
