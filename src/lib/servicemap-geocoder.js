2caf7012b7a5d4e67c974f34cc174caf63214f7e
Timo Tuominen <tituomin@gmail.com>
2016-11-03 10:03:03 +0200

    Fix streetname without number

--- src/lib/servicemap-geocoder.js
+++ src/lib/servicemap-geocoder.js
@@ -69,7 +69,7 @@ function parseAddressString(addressString) {
   if (numberIndex > 0) {
     streetName = remainingTokens.slice(0, numberIndex).join(' ');
   } else {
-    streetName = remainingTokens;
+    streetName = remainingTokens[0];
   }
   const number = numericPrefix(remainingTokens[numberIndex]);
   return {
